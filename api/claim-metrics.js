8de6fdb80e4fece9c0d50fd917e3b4932bad45f4
vinsurg <vmehan@gmail.com>
2025-10-09 14:32:13 -0400

    Update claim-metrics.js

--- api/claim-metrics.js
+++ api/claim-metrics.js
@@ -1,5 +1,6 @@
 // Vercel Serverless Function: /api/claim-metrics
-import { Pool } from "pg";
++ import pkg from "pg";
++ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
